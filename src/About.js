9090267765f76436d2734708b5fcab3052ae9e93
Nehal lopes <nehallopes@gmail.com>
2024-09-12 19:31:09 -0500

    Commit

--- src/About.js
+++ src/About.js
@@ -43,7 +43,13 @@ const About = () => {
         <h2>About <span>Me</span></h2>
         <div className="about-card">
           <p>
-            As a passionate Computer Science graduate student at DePaul University, I am actively seeking full-time opportunities in Software Development with a focus on web and mobile applications. With a solid foundation in React.js, Angular, JavaScript, HTML, CSS, and JSON, I bring 1.5 years of hands-on experience at Accenture, where I specialized in full-stack development using React.js, Node.js, and MySQL. My technical expertise also extends in developing iOS and Android applications with Swift and Java. I am adept at problem-solving, delivering high-quality solutions, and collaborating effectively with cross-functional teams. I am eager to apply my skills and experience to contribute to innovative and impactful projects.
+          As a passionate Computer Science graduate student at DePaul University, I am actively seeking full-time opportunities in Software Development, with a focus on both web and mobile applications. My expertise spans front-end and back-end technologies, with hands-on experience in React.js, Angular, JavaScript, HTML, CSS and Node.js. With 1.5 years at Accenture, I specialized in full-stack development using React.js, Node.js, and MongoDB, designing responsive and high-performing web applications.
+          </p>
+          <p>
+          In addition to my web development experience, I have a strong foundation in iOS and Android development, having built mobile applications with Swift and Java. My technical skills extend to automating CI/CD pipelines, improving deployment efficiency, and delivering secure, scalable solutions in industries such as healthcare, education, and consumer sectors.
+          </p>
+          <p>
+          With a deep understanding of Agile methodologies, I am adept at problem-solving, delivering high-quality solutions, and collaborating effectively with cross-functional teams. I am eager to apply my technical expertise in web and mobile application development to contribute to innovative and impactful projects that drive real-world results.          
           </p>
         </div>
         <div className="about-buttons">
